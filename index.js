978f20f36b9bc8f6f0c611bffea17ab4d8109bb3
piwkodev <piotrwrobeldev@gmail.com>
2021-02-24 14:36:47 +0000

    working on templates

--- index.js
+++ index.js
@@ -11,90 +11,67 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// const CLIENT_ID = process.env.CLIENT_ID
-// const CLIENT_SECRET = process.env.CLIENT_SECRET 
-// const REDIRECT_URI = process.env.REDIRECT_URI
-// const REFRESH_TOKEN = process.env.REFRESH_TOKEN
-
-// const oAuth2Client = new google.auth.OAuth2( CLIENT_ID, CLIENT_SECRET, REDIRECT_URI )
-
-// oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
-
-const mailTemaplate =  ( name, message, email) => `
-    <h1 style="color: #ff3e00">You have new massage from protfolio website</h1> 
-    <p>From: <span style="text-transform: uppercase">${name}</span></p>
-    <code>
-        ${message}
-    </code>
-    <br />
-    <h3>Contact details</h3>
-    <p>Name: ${name}</p>
-    <p>Email: ${email}</p>
-`
-
-const confirmationTemplate = ( name ) =>  `
-<br />
-<h1 style="padding: 10px 0">Thank you  <span style="text-transform: uppercase">${name}</span>! I got your message</h1>
-I will be back to you as soon as possible <br />
-If you could let me know how did you like my portfolio <a href="#" style="color: #ff3e00;">here.</a> <br />
-See you soon!
-<br />
-<h4 style="color:  #ff3e00">Peter Sparrow Portfolio</h4>
-    <br />
-    Website: https://peter-sparrow.dev
-    <br />
-
-    Email: piotrekwrobel93@gmail.com
-    <br />
-
-    Telephone: 07412 290 264
-    <br />
-    Github: https://github.com/piotrekwrobel93
-`
-
-async function sendMail( reciever, template ) {
-    try {
-        const ACCESS_TOKEN = await oAuth2Client.getAccessToken()
-        const transport = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.MYEMAIL,
-                password: process.env.PASSWORD
-                // type: 'OAuth2',
-                // user: process.env.MYEMAIL,
-                // clientId: CLIENT_ID,
-                // clientSecret: CLIENT_SECRET,
-                // refreshToken: REFRESH_TOKEN,
-                // accessToken: ACCESS_TOKEN
-
-            }
-        })
 
 
-        const mailOptions = {
-            from: `Peter Sparrow <${process.env.MYEMAIL}>`,
-            to: reciever,
-            subject: "Portfolio Contact Submition",
-            text: template,
-            html: template
+const templateToClient = (name) => `
+    <div>
+        <h1>Thank you ${name}</h1>
+        <br />
+        <h4>I recieved your message and will get back to you ASAP.</h4>
+        <h4>Please review my website so i know what other thinks about it and how to improve it</h4>
+        <h4>https://sparrow-test/review</h4>
+        <br />
+        <h5>Details:</h5>
+        <p>Name: Piotr Wrobel (Peter Sparrow)</p>
+        <p>Contact Number: 07521 220 446</p>
+        <p>Email: <span>${process.env.MYEMAIL}</span></p>
+        <p>GitHub: https://github.com/piotrekwrobel93</p>
+        <p>Website: https://sparrow-test.netlify.app</p>
+    </div>
+    `
+const templateToMe = (name, email, message) => `
+    <div>
+        <h1>You have new message from ${name}</h1>
+        <br />
+        <pre style="background-color: #ccc; color: #000;padding: 10px;">
+            ${message}
+        </pre>
+        <br />
+        <h5>Details:</h5>
+        <p>Name: ${name}</p>
+        <p>Email: <span>${email}</span></p>
+    </div>
+    `
+function sendMail( client, template ) {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MYEMAIL,
+            pass: 'konradek2'
         }
-        const mailResult = await transport.sendMail( mailOptions )
-        return mailResult
-        } catch(error) {
-            throw new Error(error.message)
-        }
+    })
+
+    const mailOptions = {
+        from: process.env.MYEMAIL,
+        to: client,
+        subject: 'Peter Sparrow - Thank you!',
+        html: template
+    }
+
+
+    transporter.sendMail( mailOptions, function(err, data) {
+        if (err) { return console.log("error occured: ", err) }
+        console.log("Email sent!", data)
+    })
 }
 
-app.get('/', (req, res) => res.send("hello there :)"))
-app.get('/test', (req,res) => res.send("this is test!"))
+
 
 app.post("/sendMail", ( req, res) => {
-    // const { name, email, message } = req.body
-    sendMail(process.env.__EMAIL__, mailTemaplate("Petro", "test test test", "petersparrow27@gmail.com")).then( res => console.log("EMAIL SENT WITH DETAILS --------- [SENT]", res)
-        .catch(err => console.log("there was an error in sending mail : ", err.message)))
-    // sendMail(email, confirmationTemplate( name )).then( res => console.log("EMAIL SENT TO CLIENT --------- [SENT]", res))
-        // 
-    res.status(200).send({status:'ok'})
+    const { name, email, message} = req.body
+    sendMail(req.body.email, templateToClient(name))
+    sendMail(process.env.MYEMAIL, templateToMe(name,email,message))
+    res.send({isOK: true, reciever: req.body.email}).status(200)
 })
 
 
